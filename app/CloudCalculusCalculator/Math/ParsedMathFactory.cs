971cc854246f2583db2cf65bea96d215e1f37b9c
Jaime Shirazi <73824309+JaimeShirazi@users.noreply.github.com>
2025-10-23 12:40:16 +1100

    refactor: improve class responsibility distribution 2 (#5)
    Moved static function to new static class

---
+++ app/CloudCalculusCalculator/Math/ParsedMathFactory.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace CloudCalculusCalculator.Math
+{
+    public static class ParsedMathFactory
+    {
+        public static ParsedMath Create(string json)
+        {
+            JToken root = JToken.Parse(json);
+
+            if (root is not JArray arr)
+            {
+                throw new FormatException("Input was not an array like expected.");
+            }
+
+            return arr[0].Value<string>() switch
+            {
+                "Equal" => new ParsedEquality(arr),
+                _ => new ParsedExpression(arr)
+            };
+        }
+    }
+}
