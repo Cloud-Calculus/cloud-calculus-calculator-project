1b3b840da072a5138d8fe954a2b39bbc2795aa45
Lei <165124161+Leidaans@users.noreply.github.com>
2025-10-24 00:03:50 +1100

    test: implemented nunit framework (#26)

---
+++ app/CloudCalculusCalculator.Tests/AdditionTests.cs
@@ -0,0 +1,16 @@
+﻿namespace CloudCalculusCalculator.Tests;
+
+public class AdditionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
